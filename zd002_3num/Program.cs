4b5c7a3c1ff315c37ce9a7e3829e3cf6e38390d7
[YanaB] <[tudabae@gmail.com]>
2023-06-26 19:12:44 +0300

    задача про третье число в разных цифрах

---
+++ zd002_3num/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.Write("третьего числа не существует");
+}
+
+int thirdnum(int num){
+while (num > 999){
+    num /= 10;
+}
+return num % 10;
+}
+Console.WriteLine(thirdnum(num));
+
+
