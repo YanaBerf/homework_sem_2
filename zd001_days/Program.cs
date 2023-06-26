b70c1c579ba21e383dbd6f7382892ed05ebb8f92
[YanaB] <[tudabae@gmail.com]>
2023-06-26 19:11:37 +0300

    задача дни недели

---
+++ zd001_days/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (day >= 1 && day <= 5)
+{
+        Console.WriteLine("будний день");
+}
+else
+{
+    Console.WriteLine("такого дня недели не существует");
+}
